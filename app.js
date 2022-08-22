6fde47139810ada452af86ef5947d98e8588f1f6
Du-Wei-Yi <tommy3007@hotmail.com>
2022-08-23 00:15:43 +0800

    Import restaurant.json to render partial template through each method at index.handlebars

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json")
 const port = 3000;
-
+// 2
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
+// 3 
 app.use(express.static("public"))
+// 1 
 app.get("/", (req, res) => {
-  res.render('index')
+  res.render('index', { restaurant: restaurantList.results })
 })
 
 
